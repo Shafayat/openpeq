e7051f2ce446b4dca62305f8d4155c9380a05ffb
Shahjada <imsobuj@gmail.com>
2026-05-21 17:50:39 +0600

    Revert "Add HID diagnostic page for Crinear analog gain hunt"
    This reverts commit d3c1ae5dbb8abf0e393e99091b0c54a8a1e6eff9.

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { DiagnosticApp } from './diagnostic/DiagnosticApp'
-
-const isDiagnostic = new URLSearchParams(window.location.search).has('diagnostic');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {isDiagnostic ? <DiagnosticApp /> : <App />}
+    <App />
   </StrictMode>,
 )
